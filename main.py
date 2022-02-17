4b0f45b8b81efc8802b366b1cc3a8826fb038595
Iaroalav <65036611+YarMolenko13@users.noreply.github.com>
2022-02-17 09:59:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
 # TODO: Frontend: unclickable send files
 
-# TODO: удаление папки при выходе с results
+# TODO: удаление папки при выходе с results +/-
 
 # TODO: логирование
 
